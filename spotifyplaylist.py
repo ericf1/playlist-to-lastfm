f95de7dccd85c92d7bdf33593e29d3acc50a9953
Eric <89803837+ericf1@users.noreply.github.com>
2024-03-02 13:49:58 -0600

    now to vectorize the similarity

--- spotifyplaylist.py
+++ spotifyplaylist.py
@@ -3,11 +3,12 @@ import pandas as pd
 import numpy as np
 import os
 from dotenv import load_dotenv
+import jellyfish
 
 load_dotenv()
 
 token = os.getenv("TOKEN")
-playlist_link = "https://open.spotify.com/playlist/2D2r0Wi6PxYvYHvCsQ96tt?si=3ed05decfe20476f"
+playlist_link = "https://open.spotify.com/playlist/2D2r0Wi6PxYvYHvCsQ96tt?si=5458d57f1c234572"
 
 playlist_id = playlist_link.split("/")[4].split("?")[0]
 
@@ -58,7 +59,32 @@ df2["compare_str"] = vf(df2["compare_str"])
 df2 = df2.drop("names", axis=1)
 df2 = df2.drop("artists", axis=1)
 
-final_df = pd.merge(df, df2, how="left", on="compare_str")
-final_df = final_df.sort_values(by=['playcounts'])
-final_df = final_df.drop("compare_str", axis=1)
-final_df.to_csv("toConvertToPlaylist.csv")
+
+# ok this is actually probably a useful implementation that I will use in the future
+# Goal: join two columns by string similarity maximum similarity
+final_names = []
+final_artists = []
+final_playcounts = []
+final_ids = []
+
+for i, r in df.iterrows():
+    max_sim = -1
+    final_names.append(r["names"])
+    final_artists.append(r["artists"])
+    final_ids.append(r["ids"])
+    for i, r2 in df2.iterrows():
+        sim = jellyfish.jaro_similarity(r["compare_str"], r2["compare_str"])
+        if sim == 1:
+            potential_final_playcount = r2["playcounts"]
+            break
+        if sim > max_sim:
+            max_sim = sim
+            potential_final_playcount = r2["playcounts"]
+    final_playcounts.append(potential_final_playcount)
+
+print(final_playcounts)
+
+# final_df = pd.merge(df, df2, how="left", on="compare_str")
+# final_df = final_df.sort_values(by=['playcounts'])
+# final_df = final_df.drop("compare_str", axis=1)
+# final_df.to_csv("toConvertToPlaylist.csv")
